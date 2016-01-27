4339ca95774a4ff46ea3334165132bfbe5feb620
MattSainz <Matthias.Sainz@colorado.edu>
2016-01-26 19:18:11 -0700

    fixed signup

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -4,26 +4,9 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 	function($scope, $http, $location, Authentication, $mdDialog) {
 		$scope.authentication = Authentication;
 
-		// If user is signed in then redirect back home
-		if ($scope.authentication.user) $location.path('/');
-
 		$scope.signup = function() {
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				/*
-				$mdDialog.show(
-                    $mdDialog.alert()
-                        .parent(angular.element(document.querySelector('#popupContainer')))
-                        .clickOutsideToClose(true)
-                        .title('New User Created')
-                        .content(res.data.message)
-                        .ariaLabel('You just created a new user')
-                        .ok('Got it!')
-                        .targetEvent(ev)
-                );
-                */
-				// And redirect to the index page
-				//$location.path('/networks');
+				$location.path('/networks');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
